0a1e3383fc82ea3488e7bdb83fc21edec4d46741
binodsapkota <binodsapkota96@gmail.com>
2025-05-03 10:02:54 +0545

    Controll Statement, Loops, Operators, Type Casting

--- April28.CSharpFundametals/ConrolStatement.cs
+++ April28.CSharpFundametals/ConrolStatement.cs
@@ -10,11 +10,11 @@ namespace April28.CSharpFundametals
     {//ctor => shortcut for constructor
         public ControlStatement()
         {
-            
+
         }
 
-        public void IfElseStatement(int num=0,int num2=0)//params=> parameter
-                                              //int num=0 optional parameters
+        public void IfElseStatement(int num = 0, int num2 = 0)//params=> parameter
+                                                              //int num=0 optional parameters
         {
 
             bool isPositive = num % 2 == 0;//if reminder is zero
@@ -26,6 +26,52 @@ namespace April28.CSharpFundametals
             {
                 Console.WriteLine("Negetive number");
             }
+        }
+        public void SwitchExample(int day=0)
+        {
+            
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6: Console.WriteLine("Friday");
+                    break;
+                    case 7:
+                        Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Code");
+                    break;
+            }
+        }
+
+        public int FindSmall(int a, int b)
+        {
+            bool condition = a > b;//B is small
+
+            if (condition)
+            {
+                return b;
+            }
+            else
+            {
+                return a;
+            }
+
+            
         }
     }
 }
--- April28.CSharpFundametals/Program.cs
+++ April28.CSharpFundametals/Program.cs
@@ -1,11 +1,22 @@
 ﻿using April28.CSharpFundametals;
+using System.Net.Http.Json;
 
 internal class Program
 {
     //this is entry point of the application
     private static void Main(string[] args)
     {
-        ControlStatement controlStatement = new ControlStatement();
-        controlStatement.IfElseStatement(num2:5);//argument//named argument  num2:5
+        //ControlStatement controlStatement = new ControlStatement();
+        //controlStatement.IfElseStatement(num2:5);//argument//named argument  num2:5
+
+        //controlStatement.SwitchExample(4);
+
+        //write a condition to find the smallest number among two
+        // a and b
+
+        //get user input
+        
+        Loops loops = new Loops();
+        loops.PrimeNumber();
     }
 }
