a38b937bef00421d7fa3ada7bd4b1ba1e7d4b584
binodsapkota <binodsapkota96@gmail.com>
2025-05-13 18:30:56 +0545

    localdb

--- EFCoreChapter/StudentDbContext.cs
+++ EFCoreChapter/StudentDbContext.cs
@@ -20,9 +20,9 @@ namespace EFCoreChapter
         //we need a connection string to connect to the database
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=.;Database=StudentDb;Integrated Security=true;TrustServerCertificate=true;");
+            //optionsBuilder.UseSqlServer("Server=.;Database=StudentDb;Integrated Security=true;TrustServerCertificate=true;");
             //for local db 
-            //optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Lenovo\\source\\repos\\April28.CSharpFundametals\\EFCoreChapter\\Data\\SchoolDb.mdf;Integrated Security=True;Connect Timeout=30");
+            optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Lenovo\\source\\repos\\April28.CSharpFundametals\\EFCoreChapter\\Data\\SchoolDb.mdf;Integrated Security=True;Connect Timeout=30");
         }
     }
 }
